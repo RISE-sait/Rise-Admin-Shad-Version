ea628b0288c0e287439e1b871f6b492257c8f579
KlintLee115 <klintlee1@gmail.com>
2025-02-02 01:12:56 -0700

    resolved hydration error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
+import ThemeProvider from "@/components/theme-provider";
 
 import { AppSidebar } from "@/components/app-sidebar"
 import { SidebarInset, SidebarProvider, SidebarTrigger, } from "@/components/ui/sidebar"
@@ -31,11 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider  
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange >
+        <ThemeProvider>
           <SidebarProvider>
             <AppSidebar />
             <SidebarInset>
--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,10 +2,24 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
+import dynamic from "next/dynamic"
 
-export function ThemeProvider({
+function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
+
+export default dynamic(() => Promise.resolve(({ children }: { children: React.JSX.Element }) => (
+  <ThemeProvider
+    attribute="class"
+    defaultTheme="system"
+    enableSystem
+    disableTransitionOnChange >
+    {children}
+  </ThemeProvider>
+)), {
+  ssr: false,
+}
+)
