c98bda26e2d6ce365afbf331dbf8859536bf6f89
KlintLee115 <klintlee1@gmail.com>
2025-03-06 03:12:08 -0700

    updated firebase config to use rise config

--- configs/firebase.ts
+++ configs/firebase.ts
@@ -1,5 +1,4 @@
 // Import the functions you need from the SDKs you need
-import EnvConfigs from "@/config";
 import { FirebaseOptions, getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -7,14 +6,15 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig: FirebaseOptions = {
-  apiKey: EnvConfigs.firebase.apiKey,
-  authDomain: EnvConfigs.firebase.authDomain,
-  projectId: EnvConfigs.firebase.projectId,
-  storageBucket: EnvConfigs.firebase.storageBucket,
-  messagingSenderId: EnvConfigs.firebase.messagingSenderId,
-  appId: EnvConfigs.firebase.appId,
-  measurementId: EnvConfigs.firebase.measurementId,
-}
+  apiKey: "AIzaSyC3asIejQ5bP-29GhIZIO4CnlAZO0wETqQ",
+  authDomain: "sacred-armor-452904-c0.firebaseapp.com",
+  projectId: "sacred-armor-452904-c0",
+  storageBucket: "sacred-armor-452904-c0.firebasestorage.app",
+  messagingSenderId: "461776259687",
+  appId: "1:461776259687:web:558026e90baef5a63522c2",
+  measurementId: "G-9YPMC5DDB2"
+};
+
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
 
