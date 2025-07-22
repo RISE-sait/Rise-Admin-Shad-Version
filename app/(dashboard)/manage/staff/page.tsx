39d89d7bae0b0d0dc41d2fe0408c5284e779716b
Kyle Guenter <119544443+KJG19@users.noreply.github.com>
2025-07-21 20:17:17 -0600

    Merged Staff page and Pending staff page into one (#66)

--- app/(dashboard)/manage/staff/page.tsx
+++ app/(dashboard)/manage/staff/page.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { getAllStaffs } from "@/services/staff";
 import StaffPage from "@/components/staff/StaffPage";
+import PendingStaffContainer from "@/components/pending-staff/PendingStaffContainer";
 import RoleProtected from "@/components/RoleProtected";
 
 export default async function Page() {
   const staffs = await getAllStaffs();
   return (
     <RoleProtected>
-      <StaffPage staffs={staffs} />
+      <div className="flex flex-col">
+        <StaffPage staffs={staffs} />
+        <PendingStaffContainer />
+      </div>
     </RoleProtected>
   );
 }
