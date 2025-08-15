0733b82021906885f058dae4b1ca1f51614350fd
KJG19 <kyleguenter19@gmail.com>
2025-08-15 10:42:34 -0600

    Added coach roles to teams, games and practices so Coach users can see those pages

--- app/(dashboard)/manage/teams/page.tsx
+++ app/(dashboard)/manage/teams/page.tsx
@@ -7,7 +7,7 @@ export default async function Page() {
   const teams = await getAllTeams();
 
   return (
-    <RoleProtected allowedRoles={[StaffRoleEnum.ADMIN]}>
+    <RoleProtected allowedRoles={[StaffRoleEnum.ADMIN, StaffRoleEnum.COACH]}>
       <div className="flex">
         <TeamsPage teams={teams} />
       </div>
