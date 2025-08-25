4a114cea7730d04719f322127fd5ac0a3a0a81c6
KJG19 <kyleguenter19@gmail.com>
2025-08-25 12:13:50 -0600

    added auto closer to program page after adding a new one, changed add program form to only have one X for closing the panel

--- components/programs/AddProgramForm.tsx
+++ components/programs/AddProgramForm.tsx
@@ -1,27 +1,29 @@
-import React, { useState } from "react";
-import { Button } from "@/components/ui/button";
-import { useToast } from "@/hooks/use-toast"
+import React from "react";
+import { useToast } from "@/hooks/use-toast";
 import DetailsForm from "./infoTabs/Details";
-import { X } from "lucide-react";
 import { createProgram } from "@/services/program";
 import { useUser } from "@/contexts/UserContext";
-import { Card, CardHeader, CardContent } from "@/components/ui/card";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { ProgramRequestDto } from "@/app/api/Api";
 import { revalidatePrograms } from "@/actions/serverActions";
 
 export default function AddProgramForm({
   levels,
-  onClose
+  onSuccess,
 }: {
-  levels: string[],
-  onClose?: () => void
+  levels: string[];
+  onSuccess?: () => void;
 }) {
-
   const { user } = useUser();
   const { toast } = useToast();
 
-  async function handleSaveAll(name: string, description: string, level: string, type: string, capacity: number) {
-
+  async function handleSaveAll(
+    name: string,
+    description: string,
+    level: string,
+    type: string,
+    capacity: number
+  ) {
     try {
       const programData: ProgramRequestDto = {
         name,
@@ -39,7 +41,7 @@ export default function AddProgramForm({
           title: "Error",
           description: `Failed to create program: ${programError}`,
           variant: "destructive",
-          status: "error"
+          status: "error",
         });
         return;
       }
@@ -47,10 +49,10 @@ export default function AddProgramForm({
       toast({
         title: "Success",
         description: "Program created successfully.",
-        status: "success"
-      })
+        status: "success",
+      });
       await revalidatePrograms();
-      if (onClose) onClose();
+      onSuccess?.();
       return;
     } catch (error) {
       console.error("Error during program creation:", error);
@@ -58,35 +60,26 @@ export default function AddProgramForm({
         title: "Error",
         description: "An error occurred. Please try again.",
         variant: "destructive",
-        status: "error"
+        status: "error",
       });
     }
-  };
+  }
 
   return (
     <Card className="shadow-sm">
-      <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
-        {onClose && (
-          <Button variant="ghost" size="sm" onClick={onClose}>
-            <X className="h-4 w-4" />
-          </Button>
-        )}
-      </CardHeader>
+      <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0"></CardHeader>
       <CardContent>
         <DetailsForm
-          program={
-            {
-              name: "",
-              description: "",
-              level: "",
-              type: "",
-              capacity: 0,
-            }
-          }
+          program={{
+            name: "",
+            description: "",
+            level: "",
+            type: "",
+            capacity: 0,
+          }}
           saveAction={handleSaveAll}
           levels={levels}
         />
-
       </CardContent>
     </Card>
   );
