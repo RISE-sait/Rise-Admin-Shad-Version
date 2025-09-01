866bc014424ad45bae9757c7c4939c52415ee4ef
KJG19 <kyleguenter19@gmail.com>
2025-09-01 10:15:57 -0600

    took out the revalidate 0 since it gave errors loading the barber page

--- app/(dashboard)/manage/staff/page.tsx
+++ app/(dashboard)/manage/staff/page.tsx
@@ -4,8 +4,6 @@ import StaffPage from "@/components/staff/StaffPage";
 import PendingStaffContainer from "@/components/pending-staff/PendingStaffContainer";
 import RoleProtected from "@/components/RoleProtected";
 
-export const revalidate = 0;
-
 export default async function Page() {
   const staffs = await getAllStaffs();
   return (
