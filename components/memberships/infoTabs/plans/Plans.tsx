fe34e39a744843e5def925c5bdb7324f367d58ad
KJG19 <kyleguenter19@gmail.com>
2025-09-18 22:02:31 -0600

    updated to 2 columns for better ux

--- components/memberships/infoTabs/plans/Plans.tsx
+++ components/memberships/infoTabs/plans/Plans.tsx
@@ -334,7 +334,7 @@ export default function PlansTab({ membershipId }: { membershipId: string }) {
   };
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-3">
       {plans.map((plan) => {
         const isActive = toggledPlanId === plan.id;
         if (isActive) {
