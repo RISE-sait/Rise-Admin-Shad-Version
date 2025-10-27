b832c4b2210386a0b33811f7e75fd5f8fd0da212
KJG19 <kyleguenter19@gmail.com>
2025-10-27 15:53:00 -0600

    allowed semi colons for descriptions on programs, memberships and credit packages

--- lib/programValidation.ts
+++ lib/programValidation.ts
@@ -2,14 +2,14 @@ export const PROGRAM_TEXT_INPUT_REGEX = /^[A-Za-z0-9 .,'&()\/\-]*$/;
 
 export const PROGRAM_TEXT_INPUT_PATTERN = "[A-Za-z0-9 .,'&()\\/-]*";
 
-export const PROGRAM_TEXT_AREA_REGEX = /^[A-Za-z0-9 .,'&()\/\-\n]*$/;
+export const PROGRAM_TEXT_AREA_REGEX = /^[A-Za-z0-9 .,'&()\/\-;\n]*$/;
 
 export const PROGRAM_TEXT_INPUT_MESSAGE =
   "Only letters, numbers, spaces, and . , ' & ( ) - / characters are allowed.";
 
 const PROGRAM_TEXT_INPUT_STRIP_REGEX = /[^A-Za-z0-9 .,'&()\/\-]/g;
 
-const PROGRAM_TEXT_AREA_STRIP_REGEX = /[^A-Za-z0-9 .,'&()\/\-\n]/g;
+const PROGRAM_TEXT_AREA_STRIP_REGEX = /[^A-Za-z0-9 .,'&()\/\-;\n]/g;
 
 export const sanitizeProgramText = (
   value: string,
